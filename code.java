cfacfd51ac00a05b9203de93baaf93267fb9e297
Ruttala Ramya <Ruttala.Ramya@Ltts.com>
2025-11-12 15:50:30 +0530

    updated java code

--- code.java
+++ code.java
@@ -1,6 +1,10 @@
 class Example{
-	public static void main(String[] args){
-		System.out.println("Hello World");
+        public static void main(String[] args){
+                System.out.println("Hello World");
+		int sum = sumOfNumbers(10,20);
+		System.out.println("Sum of two numbers are:"sum);
+        }
+        public static int sumOfNumbers(int a, int b){
+		return a+b;
 	}
 }
-
